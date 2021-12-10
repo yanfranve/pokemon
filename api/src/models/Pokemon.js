3d958d89581f6e14d23e49438e767ae20421f642
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 <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>
2021-12-09 19:54:25 -0500

    inicio del baken PI

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,43 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "pokemon",
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hp: {
+        type: DataTypes.STRING,
+      },
+      atack: {
+        type: DataTypes.STRING,
+      },
+      defense: {
+        type: DataTypes.STRING,
+      },
+      speed: {
+        type: DataTypes.STRING,
+      },
+      heigth: {
+        type: DataTypes.STRING,
+      },
+      welqht: {
+        type: DataTypes.STRING,
+      },
+      Image: {
+        type: DataTypes.STRING,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
